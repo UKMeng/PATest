0d841356233ccdd90ee430e738e004214801906f
UKMeng <myj1997118@gmail.com>
2019-01-25 23:29:16 +0800

    Update A1067.cpp

--- Aclass/A1067.cpp
+++ Aclass/A1067.cpp
@@ -1,18 +1,44 @@
-#include <iostream>
+#include <iostream>  
 #include <algorithm>
 using namespace std;
 const int maxn = 100010;
-int number[maxn];
+int pos[maxn];
 int main()
 {
     int N;
-    cin >> N;
+    scanf("%d", &N);
     int ans = 0;
+    int wrongpos = 0;
+    int temp;
     for(int i = 0; i < N; i++)
     {
-        cin >> number[i];
-        if(number[i] != i) ans += 1;
+        scanf("%d", &temp);
+        pos[temp] = i;
+        if(pos[temp] != temp && temp != 0) wrongpos++;          
     }
-    cout << ans - 1;
+    int k = 1; // 判断某个数是否在本位，回归本位后的数是不再会变化位置的
+    while(wrongpos > 0)
+    {   
+        if(pos[0] != 0) 
+        {
+            swap(pos[0], pos[pos[0]]);
+            ans++;
+            wrongpos--;
+        }
+        else if(pos[0] == 0)
+        {
+           while(k < N)
+           {
+               if(pos[k] != k)
+               {
+                   swap(pos[0], pos[k]);
+                   ans++;
+                   break;
+               }
+               k++;
+           }
+        }
+    }
+    cout << ans;
     return 0;
 }
