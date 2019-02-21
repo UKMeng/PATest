bf3983275051c95879f76460023b2a2ce24abc32
UKMeng <myj1997118@gmail.com>
2019-02-21 17:20:10 +0800

    最短路径(DIjkstra)

--- Aclass/A1003.cpp
+++ Aclass/A1003.cpp
@@ -1,57 +1,53 @@
-/*Dijkstra Algorithm*/
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int n,m,c1,c2;
-int e[510][510],weight[510],dis[510],num[510],w[510];
-bool visit[510];
-const int inf = 99999999;
-int main()
-{
-	cin>>n>>m>>c1>>c2;
-	for(int i=0;i<n;i++)
-		cin>>weight[i];
-	fill(e[0],e[0]+510*510,inf);
-	fill(dis,dis+510,inf);
-	int a,b,c;
-	for (int i=0;i<m;i++)
-	{
-		cin>>a>>b>>c;
-		e[a][b]=e[b][a]=c;
-	}
-	dis[c1]=0;
-	w[c1]=weight[c1];
-	num[c1]=1;
-	for(int i=0;i<n;i++)
-	{
-		int u=-1,minn=inf;
-		for(int j=0;j<n;j++)
-		{
-			if (visit[j]==false&&dis[j]<minn)
-			{
-				u=j;
-				minn=dis[j];
-			}
-		}
-		if(u==-1) break;
-		visit[u]=true;
-		for(int v=0;v<n;v++)
-		{
-			if(visit[v]==false&&e[u][v]!=inf)
-			{
-				if(dis[u]+e[u][v]<dis[v])
-				{
-					dis[v]=dis[u]+e[u][v];
-					num[v]=num[u];
-					w[v]=w[u]+weight[v];
-				}else if(dis[u]+e[u][v]==dis[v])
-				{
-					num[v]=num[v]+num[u];
-					if(w[u]+weight[v]>w[v]) w[v]=w[u]+weight[v];
-				}
-			}
-		}
-	}
-	printf("%d %d",num[c2],w[c2]);
-	return 0;
+const int INF = 1000000000;
+const int maxn = 510;
+int n, m, c1, c2, G[maxn][maxn], d[maxn], weight[maxn], num[maxn], w[maxn];
+bool vis[maxn];
+void Dijkstra(int s) {
+    fill(d, d + maxn, INF);
+    fill(w, w + maxn, 0);
+    fill(num, num + maxn, 0);
+    d[s] = 0;
+    num[s] = 1;
+    w[s] = weight[s];
+    for(int i = 0; i < n; i++) {
+        int u = -1, MIN = INF;
+        for(int j = 0; j < n; j++) {
+            if(vis[j] == false && d[j] < MIN) {
+                MIN = d[j];
+                u = j;
+            }
+        }
+        if(u == -1) return;
+        vis[u] = true;
+        for(int v = 0; v < n; v++) {
+            if(vis[v] == false && G[u][v] != INF) {
+                if(d[u] + G[u][v] < d[v]) {
+                    d[v] = d[u] + G[u][v];
+                    w[v] = w[u] + weight[v];
+                    num[v] = num[u];
+                }
+                else if(d[u] + G[u][v] == d[v]) {
+                    if(w[u] + weight[v] > w[v]) w[v] = w[u] + weight[v];
+                    num[v] += num[u];
+                }
+            }
+        }
+    }
+}
+int main() {
+    fill(G[0], G[0] + maxn * maxn, INF);
+    scanf("%d %d %d %d", &n, &m, &c1, &c2);
+    for(int i = 0; i < n; i++) scanf("%d", &weight[i]);
+    for(int i = 0; i < m; i++) {
+        int id1, id2, wt;
+        scanf("%d %d %d", &id1, &id2, &wt);
+        G[id1][id2] = wt;
+        G[id2][id1] = wt;
+    }
+    Dijkstra(c1);
+    cout << num[c2] << " " << w[c2];
+    return 0;
 }
