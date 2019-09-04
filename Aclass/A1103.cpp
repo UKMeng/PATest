b22e9f518ba1bc9d6ea0c9a762fb036f9f5c25ac
UKMeng <myj1997118@gmail.com>
2019-09-05 00:47:37 +0800

    update A1103

--- Aclass/A1103.cpp
+++ Aclass/A1103.cpp
@@ -1,40 +1,113 @@
-#include <iostream>
+// #include <iostream> // https://github.com/liuchuo/PAT/blob/master/AdvancedLevel_C%2B%2B/1103.%20Integer%20Factorization%20(30)%20.cpp
+// #include <vector>
+// #include <cmath>
+// using namespace std;
+// int n, k, p, maxFacSum = -1;
+// vector<int> v, ans, tempAns;
+// void init() {
+//     int temp = 0, index = 1;
+//     while (temp <= n) {
+//         v.push_back(temp);
+//         temp = pow(index, p);
+//         index++;
+//     }
+// }
+// void dfs(int index, int tempSum, int tempK, int facSum) {
+//     if (tempK == k) {
+//         if (tempSum == n && facSum > maxFacSum) {
+//                 ans = tempAns;
+//                 maxFacSum = facSum;
+//         }
+//         return;
+//     }
+//     while(index >= 1) {
+//         if (tempSum + v[index] <= n) {
+//             tempAns[tempK] = index;
+//             dfs(index, tempSum + v[index], tempK + 1, facSum + index);
+//         }
+//         if (index == 1) return;
+//         index--;
+//     }
+// }
+// int main() {
+//     scanf("%d%d%d", &n, &k, &p);
+//     init(); 
+//     tempAns.resize(k);
+//     dfs(v.size() - 1, 0, 0, 0);
+//     if (maxFacSum == -1) {
+//         printf("Impossible");
+//         return 0;
+//     }
+//     printf("%d = ", n);
+//     for (int i = 0; i < ans.size(); i++) {
+//         if (i != 0) printf(" + ");
+//         printf("%d^%d", ans[i], p);
+//     }
+//     return 0;
+// }
+#include <iostream>      // 未知原因造成一个点超时
 #include <cmath>
 #include <vector>
+#include <unordered_map>
 using namespace std;
-vector<int> v, ans, tempAns;
+vector<int> temp, ans, v;
+unordered_map<int, int> table;
 int n, k, p, maxSum = -1;
-void init() {
-    int temp = 0, index = 1;
-    while(temp <= n) {
-        v.push_back(temp);
-        temp = pow(index, p);
-        index++;
+void init2() {
+    int i = 1;
+    for(int i = 0; i < v.size(); i++) {
+        for(int j = v[i]; j < v[i + 1]; j++) {
+            table[j] = i;
+        }
     }
 }
-void dfs(int index, int la, int sum, int inSum) {
-    if(la == k) {
-        if(sum == n && inSum > maxSum) {
-            ans = tempAns;
-            maxSum = inSum;
-        }
-        return;
+void init() {
+    int t = 0, index = 1;
+    while (t <= n) {
+        v.push_back(t);
+        t = pow(index, p);
+        index++;
     }
-    while(index >= 1) {
-        if(sum + v[index] <= n) {
-            tempAns[la] = index;
-            dfs(index, la + 1, sum + v[index], inSum + index);
+    v.push_back(t);
+    init2();
+}
+void dfs(int index, int res, int sum, int premax) {
+    int start;
+    for(start = table[res]; start >= 1; start--) {
+        // cout << res << " " << start << endl;
+        if(start > premax) continue;
+        if(index != k - 1) {
+            if(v[start] < res) {
+                temp.push_back(start);
+                dfs(index + 1, res - v[start], sum + start, start);
+                temp.pop_back();
+            }
+        } else if(index == k - 1) {
+            if(v[start] == res) {
+                if(sum + start > maxSum) {
+                    temp.push_back(start);
+                    ans = temp;
+                    maxSum = sum + start;
+                    temp.pop_back();
+                } else if(sum == maxSum) {
+                    temp.push_back(start);
+                    for(int i = 0; i < k; i++) {
+                        if(temp[i] != ans[i]) {
+                            if(temp[i] > ans[i]) ans = temp;
+                            break;
+                        } 
+                    }
+                    temp.pop_back();
+                }
+            }
         }
-        if(index == 1) return;
-        index--;
     }
 }
 int main() {
     cin >> n >> k >> p;
-    tempAns.resize(k);
     init();
-    dfs(v.size() - 1, 0, 0, 0);
-    if(maxSum == -1) cout << "Impossible";
+    dfs(0, n, 0, table[n]);
+    if(maxSum == -1) cout << "Impossible" << endl;
     else {
         cout << n << " = ";
         for(int i = 0; i < k; i++) {
