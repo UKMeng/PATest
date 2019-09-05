723f3801062ed73bc60084391d75fa566eaf617a
UKMeng <myj1997118@gmail.com>
2019-09-05 23:48:06 +0800

    最短路径 - Dijkstra

--- Aclass/A1030.cpp
+++ Aclass/A1030.cpp
@@ -1,36 +1,35 @@
-#include <iostream>
-#include <vector>
+#include <iostream> // 最短路径，Dijkstra
 #include <algorithm>
 using namespace std;
-const int INF = 1000000000;
 const int maxn = 510;
-int n, m, s, z, G[maxn][maxn], d[maxn], cost[maxn][maxn], c[maxn], pre[maxn];
+const int INF = 0x7fffffff;
+int n, m, s, distination, G[maxn][maxn], cost[maxn][maxn], d[maxn], costSum[maxn], pre[maxn];
 bool vis[maxn];
-void Dijkstra(int s) {
+void dijkstra(int s) {
     fill(d, d + maxn, INF);
-    fill(c, c + maxn, INF);
+    fill(costSum, costSum + maxn, INF);
     fill(pre, pre + maxn, -1);
     d[s] = 0;
-    c[s] = 0;
+    costSum[s] = 0;
     for(int i = 0; i < n; i++) {
         int u = -1, MIN = INF;
         for(int j = 0; j < n; j++) {
-            if(vis[j] == false && d[j] < MIN) {
-                u = j;
+            if(vis[j] == false && MIN > d[j]) {
                 MIN = d[j];
+                u = j;
             }
         }
         if(u == -1) return;
         vis[u] = true;
         for(int v = 0; v < n; v++) {
             if(vis[v] == false && G[u][v] != INF) {
-                if(d[u] + G[u][v] < d[v]) {
+                if(d[v] > d[u] + G[u][v]) {
+                    costSum[v] = costSum[u] + cost[u][v];
                     d[v] = d[u] + G[u][v];
                     pre[v] = u;
-                    c[v] = c[u] + cost[u][v];
-                } else if(d[u] + G[u][v] == d[v]) {
-                    if(c[v] > c[u] + cost[u][v]) {
-                        c[v] = c[u] + cost[u][v];
+                } else if(d[v] == d[u] + G[u][v]) {
+                    if(costSum[v] > costSum[u] + cost[u][v]) {
+                        costSum[v] = costSum[u] + cost[u][v];
                         pre[v] = u;
                     }
                 }
@@ -38,24 +37,23 @@ void Dijkstra(int s) {
         }
     }
 }
-void dfs(int z) {
-    if(pre[z] != -1) dfs(pre[z]);
-    cout << z << " ";
+void dfs(int d) {
+    if(d != s) {
+        dfs(pre[d]);
+    } cout << d << " ";
 }
 int main() {
-    cin >> n >> m >> s >> z;
+    cin >> n >> m >> s >> distination;
     fill(G[0], G[0] + maxn * maxn, INF);
     fill(cost[0], cost[0] + maxn * maxn, INF);
     for(int i = 0; i < m; i++) {
-        int id1, id2, dis, ct;
-        cin >> id1 >> id2 >> dis >> ct;
-        G[id1][id2] = dis;
-        G[id2][id1] = dis;
-        cost[id1][id2] = ct;
-        cost[id2][id1] = ct;
+        int t1, t2, dis, cos;
+        cin >> t1 >> t2 >> dis >> cos;
+        G[t1][t2] = G[t2][t1] = dis;
+        cost[t1][t2] = cost[t2][t1] = cos;
     }
-    Dijkstra(s);
-    dfs(z);
-    cout << d[z] << " " << c[z] << endl;
+    dijkstra(s);
+    dfs(distination);
+    cout << d[distination] << " " << costSum[distination] << endl;
     return 0;
 }
--- README.md
+++ README.md
@@ -6,4 +6,4 @@
   * 质数判断: [A1156](https://github.com/UKMeng/PATest/blob/master/Aclass/A1156.cpp)
 * AVL树
 * 并查集
-* 最短路径(Dijkstra算法): [A1003](https://github.com/UKMeng/PATest/blob/master/Aclass/A1003.cpp) [A1018](https://github.com/UKMeng/PATest/blob/master/Aclass/A1018.cpp)
+* 最短路径(Dijkstra算法): [A1003](https://github.com/UKMeng/PATest/blob/master/Aclass/A1003.cpp) [A1018](https://github.com/UKMeng/PATest/blob/master/Aclass/A1018.cpp) [A1030](https://github.com/UKMeng/PATest/blob/master/Aclass/A1030.cpp) [A1072](https://github.com/UKMeng/PATest/blob/master/Aclass/A1072.cpp) [A1087](https://github.com/UKMeng/PATest/blob/master/Aclass/A1087.cpp)
