238eee116520353d0b422b74807f9581f236b979
UKMeng <myj1997118@gmail.com>
2021-06-15 09:14:58 +0800

    update

--- Code/1060.cpp
+++ Code/1060.cpp
@@ -7,6 +7,29 @@ pair<string, int> getPair(string str, int n){
     //
     string tempStr;
     int expo;
+    if(str.find('.') == -1) expo = str.length();
+    else expo = str.find('.');
+    int pos = 0;
+    bool flag = true;
+    for(; pos < str.size(); pos++){
+        if(str[pos] == '.') continue;
+        if(str[pos] != '0') {
+            flag = false;
+            break;
+        }
+        expo--;
+    }
+    if(flag) expo = 0;
+    for(; pos < str.size(); pos++){
+        if(tempStr.size() == n) break;
+        if(str[pos] != '.') tempStr.push_back(str[pos]);
+    }
+    if(tempStr.size() < n){
+        int l = tempStr.size();
+        for(int i = 0; i < n - l; i++){
+            tempStr.push_back('0');
+        }
+    }
     return make_pair(tempStr, expo);
 }
 
